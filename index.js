70205e155a2ee9a5dad8e232c276c48dbe2956f1
Harold Cohen Gutiérrez Baca <haroldcohen.gutierrezbaca.contractor@gdevtools.com>
2023-07-24 14:21:36 -0500

    envio de trama a rpi ok

--- index.js
+++ index.js
@@ -10,35 +10,31 @@ const io = require("socket.io")(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("nuevo socket conectado");
-});
-
-io.on("xbee:mision", (markerList)=>{
-  console.log(markerList)
-})
-
-app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
 const parser = new Readline();
-
 const xbee = new SerialPort(
-    'COM3',
+    'COM9',
     {baudRate: 57600}
 )
-
 //const xbee = new SerialPort("/dev/ttyUSB0", { baudRate: 9600 });
-
 xbee.write("INICIO GATEWAY");
-
 xbee.pipe(parser);
-
 xbee.on("open", () => {
   console.log("Puerto abierto");
+});
+
+var markerList;
+io.on("connection", (socket) => {
+  console.log("nuevo socket conectado");
+  socket.on("xbee:mision", (data)=>{
+    markerList = JSON.stringify(data);
+    xbee.write(markerList);
+  })
+});
+
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 var buffer = "";
@@ -71,11 +67,10 @@ xbee.on("data", (line) => {
   buffer += line;
   var bufferAux = buffer.split("//");
   //console.log('buffer: ' + buffer)
-
   if (bufferAux.length > 1) {
     sensors = bufferAux[0].split("/");
     buffer = buffer.replace(bufferAux[0] + "//", "");
-    console.log(sensors);
+    //console.log(sensors);
     io.emit("xbee:datos", {
       lat: sensors[0],
       lng: sensors[1],
@@ -104,7 +99,6 @@ xbee.on("data", (line) => {
       // direccion: sensors[24]
     });
   }
-
   //xbee.write('Recibido');
 });
 
